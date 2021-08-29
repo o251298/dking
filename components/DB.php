b30a3262743289fca1657a652f9782b68c850fbc
o251298 <o251298@gmail.com>
2021-08-29 11:37:34 +0300

    add charset

--- components/DB.php
+++ components/DB.php
@@ -5,7 +5,7 @@ class DB
 {
     public static function getConnection(){
         $params = include(ROOT.'/config/db_params.php');
-        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
+        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']};charset=UTF8";
         $db = new PDO($dsn, $params['user'], $params['password']);
         return $db;
     }
